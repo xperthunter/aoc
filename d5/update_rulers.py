4418e432be5168501ca52b13a2bd355419813826
xperthunter <kjfraga@ucdavis.edu>
2024-12-08 16:28:55 -0800

    day 5 done

--- d5/update_rulers.py
+++ d5/update_rulers.py
@@ -37,23 +37,34 @@ for orders in pages:
 		
 		if not correct: break
 	if correct:
-		print(orders[int((len(orders)-1)/2)])
+		#print(orders[int((len(orders)-1)/2)])
 		mids += int(orders[int((len(orders)-1)/2)])
 	else:
 		incorrects.append(orders)
 
 print(mids)
 
+mids = 0
 while len(incorrects) > 0:
-	for ii, inc in enumerate(incorrects.copy()):
+	for ii, inc in enumerate(incorrects):
 		correct = True
-		for i, pg in enumerate(inc.copy()):
+		for i, pg in enumerate(inc):
 			for j in range(i+1, len(inc)):
 				if inc[j] in rules:
-					if pg in rules[orders[j]]:
+					if pg in rules[inc[j]]:
+						#print(i, j, ii)
+						#print(incorrects[ii])
 						incorrects[ii][i], incorrects[ii][j] = incorrects[ii][j], incorrects[ii][i]
 						correct = False
 						break
+			
+			if not correct: break
+		if correct:
+			mids += int(inc[int((len(inc)-1)/2)])
+			del incorrects[ii]
+			#print(len(incorrects))
+
+print(mids)
 					
 					
 					
