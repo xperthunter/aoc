fe074e1777d28c1d88463fbdb896fc8609f872b3
xperthunter <kjfraga@ucdavis.edu>
2025-04-14 13:36:55 -0700

    cleaned up day 7

--- d7/operators.py
+++ d7/operators.py
@@ -1,6 +1,54 @@
 #!/usr/bin/python3
 
+import json
 import sys
+
+
+def correct_operators(tests, operations):
+	
+	signs = operations
+	start_ops = [[op] for op in operations]
+	correct = 0
+	
+	for i, test in enumerate(tests):
+		ops = start_ops.copy()
+		for i in range(1,len(test['terms'])-1):
+			
+			for op in ops.copy():
+				for sign in signs:
+					new = op+[sign]
+					ops.append(new)
+			
+			ops = [op for op in ops if len(op) == i+1]
+		
+		if len(ops) != (len(signs)**(len(test['terms'])-1)):
+			print('missing')
+			print(ops)
+			print(len(test['terms']))
+			print(len(signs))
+			print(len(ops))
+			print(len(signs)**(len(test['terms'])-1))
+			print(json.dumps(test,indent=2))
+			sys.exit()
+		
+		for order in ops:
+			result = test['terms'][0]
+			for i, (op,term) in enumerate(zip(order,test['terms'][1:])):
+				
+				if op == '+': result += term
+				elif op == '*': result *= term
+				elif op == '|': result = int(str(result)+str(term))
+				else:
+					print(op)
+					sys.exit()
+			
+			if result == test['ans']:
+				correct += test['ans']
+				break
+	
+	return correct
+	
+
 
 with open(sys.argv[1], 'r') as fp:
 	eqs = []
@@ -21,60 +69,13 @@ for eq in eqs:
 	
 	tests.append(eqn)
 
-#sys.exit()
-signs = ['+', '*', '|']
-correct = 0
-for i, test in enumerate(tests):
-	
-# 	minimum = 0
-# 	for term in test['terms']: minimum += term
-# 	maximum = 1
-# 	for term in test['terms']: maximum *= term
-	
-# 	if test['ans'] < minimum and test['ans'] > maximum:
-# 		print(minimum)
-# 		print(maximum)
-# 		print(f"{i+1} answer: {test['ans']}")
-# 	#continue
-	
-	ops = [['+'],['*'], ['|']]
-	for i in range(1,len(test['terms'])-1):
-		
-		for op in ops.copy():
-			for sign in signs:
-				new = op+[sign]
-				ops.append(new)
-		
-		ops = [op for op in ops if len(op) == i+1]
-	
-	print()
-	print(len(ops))
-	print(3**(len(test['terms'])-1))
-	print(test['terms'])
-	print()
-	
-	if len(ops) != (3**(len(test['terms'])-1)):
-		print('missing')
-		print(ops)
-		sys.exit()
-	
-	for order in ops:
-		result = test['terms'][0]
-		for i, (op,term) in enumerate(zip(order,test['terms'][1:])):
-			
-			if op == '+': result += term
-			elif op == '*': result *= term
-			elif op == '|': result = int(str(result)+str(term))
-			else:
-				print(op)
-				sys.exit()
-		
-		if result == test['ans']:
-			correct += test['ans']
-			print(test['terms'])
-			print(test['ans'])
-			print(order)
-			break
+signs = ['+', '*']
+correct = correct_operators(tests, signs)
+print(f'Part 1: {correct}')
 
-print(correct)
+
+signs = ['+', '*', '|']
+correct = correct_operators(tests, signs)
+print(f'Part 2: {correct}')
+
 			 
