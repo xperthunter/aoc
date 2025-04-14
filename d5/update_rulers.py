379128dfb57b9d209c0045e735d6fba515e685e3
xperthunter <kjfraga@ucdavis.edu>
2025-04-14 12:55:26 -0700

    small updates

--- d5/update_rulers.py
+++ d5/update_rulers.py
@@ -17,18 +17,12 @@ with open(sys.argv[1], 'r') as fp:
 			order = line.split(',')
 			pages.append(order)
 
-# for k1 in rules.keys():
-# 	for k2 in rules[k1].keys():
-# 		print(k1,k2)
-
-
+# Part 1
 mids = 0
 incorrects = list()
 for orders in pages:
 	correct = True
 	for i, pg in enumerate(orders):
-		#if pg not in rules: continue
-		
 		for j in range(i+1, len(orders)):
 			if orders[j] in rules:
 				if pg in rules[orders[j]]:
@@ -37,13 +31,13 @@ for orders in pages:
 		
 		if not correct: break
 	if correct:
-		#print(orders[int((len(orders)-1)/2)])
 		mids += int(orders[int((len(orders)-1)/2)])
 	else:
 		incorrects.append(orders)
 
-print(mids)
+print(f'Part 1: mids')
 
+# Part 2
 mids = 0
 while len(incorrects) > 0:
 	for ii, inc in enumerate(incorrects):
@@ -52,8 +46,6 @@ while len(incorrects) > 0:
 			for j in range(i+1, len(inc)):
 				if inc[j] in rules:
 					if pg in rules[inc[j]]:
-						#print(i, j, ii)
-						#print(incorrects[ii])
 						incorrects[ii][i], incorrects[ii][j] = incorrects[ii][j], incorrects[ii][i]
 						correct = False
 						break
@@ -62,9 +54,8 @@ while len(incorrects) > 0:
 		if correct:
 			mids += int(inc[int((len(inc)-1)/2)])
 			del incorrects[ii]
-			#print(len(incorrects))
 
-print(mids)
+print(f'Part 2: {mids}')
 					
 					
 					
