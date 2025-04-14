a8bd903ab5525a3f4ceac2f87cbd31778dc6ceb7
xperthunter <kjfraga@ucdavis.edu>
2025-04-14 12:20:26 -0700

    small updates

--- d2/safe_reports.py
+++ d2/safe_reports.py
@@ -11,9 +11,9 @@ def safe_report(report, tolerate=False):
 	
 	if len(duplicates) > 1: return False
 	elif len(duplicates) == 1:
-		if len(uniq[duplicates[0]]) > 2: return False
+		if len(uniq[duplicates[0]]) > 2: return False 
 		else:
-			if tolerate:
+			if tolerate: # needed for part 2
 				modified = report.copy()
 				del modified[uniq[duplicates[0]][0]]
 				if safe_report(modified, tolerate=False):
@@ -33,7 +33,7 @@ def safe_report(report, tolerate=False):
 		elif report[0] - report[1] <= 3 and report[0] - report[1] > 0:
 			direction = -1
 		else:
-			if tolerate:
+			if tolerate: # needed for part 2
 				modified = report.copy()
 				del modified[0]
 				if safe_report(modified, tolerate=False):
@@ -50,7 +50,7 @@ def safe_report(report, tolerate=False):
 		 
 		for i, lvl in enumerate(report[:-1]):
 			if direction*(report[i+1] - lvl) > 3 or direction*(report[i+1] - lvl) < 0:
-				if tolerate:
+				if tolerate: # needed for part 2
 					modified = report.copy()
 					del modified[i]
 					if safe_report(modified, tolerate=False):
@@ -72,10 +72,6 @@ def safe_report(report, tolerate=False):
 	return True	
 		
 
-				
-				
-
-
 with open(sys.argv[1], 'r') as fp:
 	reports = []
 	for line in fp.readlines():
@@ -87,198 +83,27 @@ with open(sys.argv[1], 'r') as fp:
 		reports.append(levels)
 
 
+# Part 1
 safe_reports = 0
 for report in reports:
 	
-	if safe_report(report, tolerate=True):
-		print(f"safe: {' '.join([str(item) for item in report])}")
+	if safe_report(report, tolerate=False):
+#		print(f"safe: {' '.join([str(item) for item in report])}")
 		safe_reports += 1
 	else:
-		print(f"unsafe: {' '.join([str(item) for item in report])}")
+		continue
+#		print(f"unsafe: {' '.join([str(item) for item in report])}")
 
-print(safe_reports)
+print(f'Part 1: {safe_reports}')
 
 
-
-"""# 	sorted_lvs = sorted(report, reverse=False)
-# 	
-# 	safe = True
-# 	tol  = True
-# 	for i, (lvl, svl) in enumerate(zip(report[:-1], sorted_lvs[:-1])):
-# 		print(lvl, svl, tol, safe)
-# 		if lvl in report[:i]:
-# 			if tol:
-# 				if i > 0:
-# 					if report[i+1] - report[i-1] > 3 or report[i+1] - report[i-1] == 0:
-# 						print('1 nope')
-# 						safe = False
-# 						break
-# 					else:
-# 						tol = False
-# 						continue
-# 				else:
-# 					print('2 nope')
-# 					safe = False
-# 					break
-# 			else:
-# 				print('3 nope')
-# 				safe = False
-# 				break
-# 		else:
-# 			if report[i+1] - lvl > 3 or lvl - report[i+1] == 0:
-# 				
-# 				if tol:
-# 					if i > 0:
-# 						if report[i+1] - report[i-1] > 3 or report[i+1] - report[i-1] == 0:
-# 							print('4 nope')
-# 							safe = False
-# 							break
-# 						else:
-# 							tol = False
-# 							continue
-# 					else:
-# 						print('5 nope')
-# 						safe = False
-# 						break
-# 				else:
-# 					print('6 nope')
-# 					safe = False
-# 					break
-# 	if safe:
-# 		safe_reports += 1
-# 		print('safe:', ' '.join([str(lvl) for lvl in report]))
-# 	else:
-# 		print('unsafe:', ' '.join([str(lvl) for lvl in report]))
-# 
-# else:
-# 	sorted_lvs = sorted(report, reverse=True)
-# 	
-# 	safe = True
-# 	tol  = True
-# 	for i, (lvl, svl) in enumerate(zip(report[:-1], sorted_lvs[:-1])):
-# 		print(lvl, svl, tol, safe)
-# 		if lvl in report[:i]:
-# 			if tol:
-# 				if i > 0:
-# 					if report[i-1] - report[i+1] > 3 or report[i-1] - report[i+1] == 0:
-# 						print('8 nope')
-# 						safe = False
-# 						break
-# 					else:
-# 						tol = False
-# 						continue
-# 				else:
-# 					print('9 nope')
-# 					safe = False
-# 					break
-# 			else:
-# 				print('10 nope')
-# 				safe = False
-# 				break
-# 		else:
-# 			if lvl - report[i+1] > 3 or lvl - report[i+1] == 0:
-# 				
-# 				if tol:
-# 					if i > 0:
-# 						if report[i-1] - report[i+1] > 3 or report[i-1] - report[i+1] == 0:
-# 							safe = False
-# 							break
-# 						else:
-# 							tol = False
-# 							continue
-# 					else:
-# 						safe = False
-# 						break
-# 				else:
-# 					safe = False
-# 					break
-# 	
-# 	if safe:
-# 		safe_reports += 1
-# 		print('safe:', ' '.join([str(lvl) for lvl in report]))
-# 	else:
-# 		print('unsafe:', ' '.join([str(lvl) for lvl in report]))
-
-			if direction is not None:
-				
-		if report[-1] - report[0] > 0:
-		#print('ascending')
-		sorted_lvs = sorted(report, reverse=False)
-		
-		for i, (lvl, svl) in enumerate(zip(report[:-1], sorted_lvs[:-1])):
-			if lvl != svl:
-				if tolerate:
-					modified = report.copy()
-					del modified[i]
-					if safe_report(modified, tolerate=False):
-						return True
-					else:
-						modified = report.copy()
-						modified.remove(svl)
-						if safe_report(modified, tolerate=False):
-							return True
-						else:
-							return False 
-				else:
-					return False
-			else:
-				if report[i+1] - lvl > 3 or lvl - report[i+1] == 0:
-					if tolerate:
-						modified = report.copy()
-						del modified[i]
-						if safe_report(modified, tolerate=False):
-							return True
-						else:
-							modified = report.copy()
-							del modified[i+1]
-							if safe_report(modified, tolerate=False):
-								return True
-							else: 
-								return False
-					else:
-						return False
-	# descending order
+safe_reports = 0
+for report in reports:
+	if safe_report(report, tolerate=True):
+#		print(f"safe: {' '.join([str(item) for item in report])}")
+		safe_reports += 1
 	else:
-		#print('descending')
-		sorted_lvs = sorted(report, reverse=True)
-		
-		for i, (lvl, svl) in enumerate(zip(report[:-1], sorted_lvs[:-1])):
-			#print(lvl, svl)
-			if lvl != svl:
-				if tolerate:
-					modified = report.copy()
-					del modified[i]
-					if safe_report(modified, tolerate=False):
-						return True
-					else:
-						modified = report.copy()
-						modified.remove(svl)
-						if safe_report(modified, tolerate=False):
-							return True
-						else:
-							return False
-				else:
-					return False
-			else:
-				if lvl - report[i+1] > 3 or lvl - report[i+1] == 0:
-					if tolerate:
-						modified = report.copy()
-						del modified[i]
-						if safe_report(modified, tolerate=False):
-							return True
-						else:
-							modified = report.copy()
-							del modified[i+1]
-							if safe_report(modified, tolerate=False):
-								return True
-							else:
-								return False
-					else:
-						return False
-	return True
+		continue
+#		print(f"unsafe: {' '.join([str(item) for item in report])}")
 
-
-
-
-
-"""
+print(f'Part 2: {safe_reports}')
