417faae445d43f3458312336614ab16c0e520c04
xperthunter <kjfraga@ucdavis.edu>
2025-04-15 18:47:28 -0700

    d20 updates

--- d20/race.py
+++ d20/race.py
@@ -6,6 +6,7 @@ import numpy as np
 def manhattan_distance(p1, p2):
 	return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
+
 def tuples(cen, radius, max_x, max_y):
 	x, y = cen
 	
@@ -17,6 +18,7 @@ def tuples(cen, radius, max_x, max_y):
 			
 			if manhattan_distance((xi,yi), (x,y)) <= radius and manhattan_distance((xi,yi), (x,y)) > 1:
 				yield (xi, yi)
+
 
 def cheats_part2(path, track):
 	
@@ -72,6 +74,7 @@ def count_path(track, start, end):
 	
 	return path
 
+
 def valid_cheats(path, track):
 	
 	skips = [(2,0), (0,2), (-2,0), (0,-2)]
@@ -94,8 +97,8 @@ def valid_cheats(path, track):
 					if track[wx,wy] == '#':
 						savings = path[(x,y)] - path[(i,j)] - 2
 						
-						print(f'from: {(i,j)} to: {(x,y)} savings: {savings}')
-						print(f'from count: {path[(i,j)]} to count: {path[(x,y)]}')
+# 						print(f'from: {(i,j)} to: {(x,y)} savings: {savings}')
+# 						print(f'from count: {path[(i,j)]} to count: {path[(x,y)]}')
 						
 						if savings not in cheats:
 							cheats[savings] = 0
@@ -114,35 +117,26 @@ with open(sys.argv[1], 'r') as fp:
 
 track = np.array(track)
 
-#print()
-#for row in track: print(''.join(row))
-#print()
-
 start = np.where(track == 'S')
 start = (start[0][0], start[1][0])
 end   = np.where(track == 'E')
 end   = (end[0][0], end[1][0])
 track_length = np.sum(track == '.')
 
-print(start)
-print(end)
-print(track_length)
+# print(start)
+# print(end)
+# print(track_length)
 
 path = count_path(track, start, end)
 
-print(path)
+cheats = valid_cheats(path, track)
 
-# cheats = valid_cheats(path, track)
-# 
-# productive_cheats = 0
-# for k,v in sorted(cheats.items(), key=lambda x: x[0]):
-# 	if k >= 100:
-# 		print(k,v)
-# 		productive_cheats += v
-# 
-# print(productive_cheats)
+productive_cheats = 0
+for k,v in sorted(cheats.items(), key=lambda x: x[0]):
+	if k >= 100:
+		productive_cheats += v
 
-
+print(f'Part 1: {productive_cheats}')
 
 # part 2
 
@@ -151,35 +145,6 @@ cheats = cheats_part2(path, track)
 productive_cheats = 0
 for k,v in sorted(cheats.items(), key=lambda x: x[0]):
 	if k >= 100:
-		print(v,k)
 		productive_cheats += v
 
-print(productive_cheats)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(f'Part 2: {productive_cheats}')
