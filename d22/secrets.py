3fa022fa996360d1451e682a07192922af788ca4
xperthunter <kjfraga@ucdavis.edu>
2025-04-20 17:03:14 -0700

    cleaned d22

--- d22/secrets.py
+++ d22/secrets.py
@@ -35,52 +35,37 @@ def evolve(num):
 	return num
 
 score = 0
+part1 = 0
 total_diffs = dict()
 for buyer, beg in enumerate(inputs):
 	last_four = [None, None, None, None]
 	diffs = dict()
-	print(buyer)
 	for i in range(2000):
 		new = evolve(beg)
-		#print(new % 10)
 		
 		diff = (new % 10) - (beg % 10)
 		last_four = last_four[1:]
 		last_four.append(diff)
-		#print(last_four)
 		if None not in last_four:
 			four_key = tuple(last_four)
 			if four_key in diffs:
 				diffs[four_key] = max(diffs[four_key], new % 10)
 			else:
 				diffs[four_key] = new % 10
-				
-			#sys.exit()
 		beg = new
 	
 	for k,v in diffs.items():
-		#print(k,v)
 		if k not in total_diffs:
 			total_diffs[k] = v
 		else:
 			total_diffs[k] += v
 	
-	
-	#score += beg
-	#sys.exit()
-
-print()
-for k,v in sorted(total_diffs.items(), key=lambda x: x[1]):
-	print(k, v)
-
-print()
+	part1 += beg
 
 most_bananas = list(sorted(total_diffs.values()))[-1]
-print(most_bananas)
 
-
-#print(score)
-	
+print(f'Part 1: {part1}')
+print(f'Part 2: {most_bananas}')	
 
 
 	
